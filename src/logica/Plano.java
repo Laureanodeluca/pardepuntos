d708fc828aa5adfef58e22b2a76fbc6b14c6a12c
SolStiep <solstiep.puan@gmail.com>
2021-05-26 16:31:59 -0300

    Arreglos en DyC

--- src/logica/Plano.java
+++ src/logica/Plano.java
@@ -2,6 +2,7 @@ package logica;
 
 import java.util.*;
 import TDALista.*;
+import java.util.concurrent.TimeUnit;
 
 public class Plano {
 
@@ -37,6 +38,10 @@ public class Plano {
 			puntos.addLast(p);
 	}
 
+	public void addLast(Punto p) {
+		puntos.addLast(p);
+	}
+
 	public PositionList<Punto> getPuntos() {
 		return puntos;
 	}
@@ -45,32 +50,34 @@ public class Plano {
 		puntos = listaPuntos;
 	}
 
-	public void mergesort(Punto a[],int izq, int der){
-		if (izq < der){
-				int m=(izq+der)/2;
-				mergesort(a, izq, m);
-				mergesort(a, m+1, der);                                                                                
-				merge(a, izq, m, der);                                                                                 
+	public void mergesort(Punto a[], int izq, int der) {
+		if (izq < der) {
+			int m = (izq + der) / 2;
+			mergesort(a, izq, m);
+			mergesort(a, m + 1, der);
+			merge(a, izq, m, der);
 		}
 	}
 
-	public void merge(Punto A[], int izq, int m, int der){
+	public void merge(Punto A[], int izq, int m, int der) {
 		int i, j, k;
-		Punto [] B = new Punto[A.length]; //array auxiliar
-		for (i=izq; i<=der; i++)      //copia ambas mitades en el array auxiliar                                       
-			 B[i]=A[i];
-	 
-		i=izq; j=m+1; k=izq;
-		  
-		while (i<=m && j<=der) //copia el siguiente elemento mÃ¡s chico                                      
-			   if (B[i].getY()<=B[j].getY())
-				   A[k++]=B[i++];
-			   else
-				   A[k++]=B[j++];
-			 
-		while (i<=m)         //copia los elementos que quedan de la
-			  A[k++]=B[i++]; //primera mitad (si los hay)
-	 }
+		Punto[] B = new Punto[A.length]; // array auxiliar
+		for (i = izq; i <= der; i++) // copia ambas mitades en el array auxiliar
+			B[i] = A[i];
+
+		i = izq;
+		j = m + 1;
+		k = izq;
+
+		while (i <= m && j <= der) // copia el siguiente elemento mÃ¡s chico
+			if (B[i].getY() <= B[j].getY())
+				A[k++] = B[i++];
+			else
+				A[k++] = B[j++];
+
+		while (i <= m) // copia los elementos que quedan de la
+			A[k++] = B[i++]; // primera mitad (si los hay)
+	}
 
 	/**
 	 * Obtiene el par de menor distancia. Chequea todos los pares de puntos.
@@ -82,7 +89,7 @@ public class Plano {
 		long ini, fin;
 		double menorDistancia = Double.MAX_VALUE;
 
-		ini = new Date().getTime();
+		ini = System.currentTimeMillis();
 
 		for (Position<Punto> p : puntos.positions()) {
 			for (Position<Punto> q : puntos.positions()) {
@@ -94,7 +101,7 @@ public class Plano {
 				}
 			}
 		}
-		fin = new Date().getTime();
+		fin = System.currentTimeMillis();
 		toReturn.setTiempo(fin - ini);
 
 		return toReturn;
@@ -114,7 +121,7 @@ public class Plano {
 		Punto p1 = new Punto();
 		Punto p2 = new Punto();
 
-		ini = new Date().getTime();
+		ini = System.currentTimeMillis();
 
 		if (cantPuntos < 2) { // Si tenemos menos de dos puntos no podemos obtener un par de puntos, por lo
 								// que se retorna la distancia mÃ¡xima
@@ -127,31 +134,22 @@ public class Plano {
 			toReturn.setP1(p1);
 			toReturn.setP2(p2);
 			toReturn.setDistancia(p1.distancia(p2));
-		} else { // Si hay 3 o mÃ¡s puntos
+		} else { // Si hay 3 o mas puntos
 			Par solIzq = new Par();
 			Par solDer = new Par();
 			Plano planoIzquierdo = new Plano();
 			Plano planoDerecho = new Plano();
 
-			int mitad = puntos.size() / 2; // Se divide el plano en dos mitades y se encuentra solucion al plano
-											// izquierdo y al plano derecho
-			planoIzquierdo.setPuntos(puntos.subList(0, mitad));
-			planoDerecho.setPuntos(puntos.subList(mitad + 1, cantPuntos));
-			// int i = 1;
-			// for (Position<Punto> p: puntos.positions())
-			// {
-			// if (i <= puntos.size()/2)
-			// ladoIzquierdo.addLast(p.element());
-			// else
-			// ladoDerecho.addLast(p.element());
-			// i++;
-			// }
+			// Se divide el plano en dos mitades y se encuentra solucion al plano izquierdo
+			// y al plano derecho
+			int mitad = puntos.size() / 2;
+			subList(planoIzquierdo, planoDerecho);
 
 			solIzq = planoIzquierdo.menorDistanciaDivCon();
 			solDer = planoDerecho.menorDistanciaDivCon();
 
-			if (solIzq.getDistancia() < solDer.getDistancia()) { // Se guarda la menor distancia entre las soluciuones
-																	// obtenidas
+			// Se guarda la menor distancia entre las soluciuones obtenidas
+			if (solIzq.getDistancia() < solDer.getDistancia()) {
 				toReturn = solIzq;
 			} else {
 				toReturn = solDer;
@@ -159,42 +157,31 @@ public class Plano {
 
 			// Creamos el plano central a partir de la franja entre los dos planos
 			// anteriores
-			// Plano planoCentral = new Plano(); ESTO SI LO HACEMOS CON LISTA
 			Punto pMedio = planoIzquierdo.getPuntos().last().element();
 
-			// HAY QUE ORDENAR LA LISTA EN BASE A LAS COORDENADAS Y EN T(n log n)
-			// HICE EL ARREGLO PARA HACER MERGE PERO PODEMOS VER SI HAY UNA FORMA MÃ�S EFICIENTE 
-			// ORDENARIA LA LISTA DIRECTAMENTE PERO NO ESTABA CON MANEJOS DE TDA PARA HACERLO
-			// ACLARACION: NO ESTOY SEGURA DEL ORDENAMIENTO. LO HICE QUE SE FIJE SOLAMENTE EN EL EJE Y. 
-			// VER SI ESTA BIEN QUE SOLO SE FIJE EN Y (YA QUE PREVIAMENTE LOS PUNTOS ESTABAN OREDENADOS SEGUN EL EJE X)
-			Punto a[]=obtenerArray();
+			// Ordenamos los puntos respecto a la coordenada Y
+			Punto a[] = obtenerArray();
 			mergesort(a, 0, a.length);
-			int i, j, count=0;
-			Punto planoCentral[]= new Punto[a.length];
-			
+			int i, j, count = 0;
+			Punto planoCentral[] = new Punto[a.length];
 
-			// Creamos una nueva lista que contendra aquellos puntos que cumplan que la
+			// Creamos un nuevo arreglo que contendra aquellos puntos que cumplan que la
 			// diferencia en valor
 			// absoluto de su componente x y la componente x del punto medio sea menor a la
 			// distancia minima
-			/*for (Position<Punto> p : puntos.positions()) {
-				if (Math.abs(p.element().getX() - pMedio.getX()) < toReturn.getDistancia()) {
-					planoCentral.addPunto(p.element());
-				}
-			}*/
-			for(i=0; i<a.length;i++){
+			for (i = 0; i < a.length; i++) {
 				if (Math.abs(a[i].getX() - pMedio.getX()) < toReturn.getDistancia()) {
-					planoCentral[i]=a[i];
+					planoCentral[i] = a[i];
 				}
 			}
 
-			//Calculamos las distancias
+			// Calculamos las distancias
 			double dist;
-			for(i=0; i<planoCentral.length; i++){
-				count=0;
-				for(j=i+1; j<planoCentral.length && planoCentral[j]!=null &&count<7; j++){
-					dist=planoCentral[i].distancia(planoCentral[j]);
-					if(dist<toReturn.getDistancia()){
+			for (i = 0; i < planoCentral.length; i++) {
+				count = 0;
+				for (j = i + 1; j < planoCentral.length && planoCentral[j] != null && count < 7; j++) {
+					dist = planoCentral[i].distancia(planoCentral[j]);
+					if (dist < toReturn.getDistancia()) {
 						toReturn.setDistancia(dist);
 						toReturn.setP1(planoCentral[i]);
 						toReturn.setP2(planoCentral[j]);
@@ -204,20 +191,31 @@ public class Plano {
 			}
 		}
 
-		fin = new Date().getTime();
+		fin = System.currentTimeMillis();
 		toReturn.setTiempo(fin - ini);
 
 		return toReturn;
 
 	}
 
-	private Punto[] obtenerArray(){
-		Punto a[]=new Punto[puntos.size()];
-		int i=0;
+	private Punto[] obtenerArray() {
+		Punto a[] = new Punto[puntos.size()];
+		int i = 0;
 		for (Position<Punto> p : puntos.positions()) {
-			a[i]=p.element();
+			a[i] = p.element();
 			i++;
 		}
 		return a;
+	}
+
+	private PositionList<Punto> subList(Plano planoIzquierdo, Plano planoDerecho) {
+		int i = 1;
+		for (Position<Punto> p : puntos.positions()) {
+			if (i <= puntos.size() / 2)
+				planoIzquierdo.addLast(p.element());
+			else
+				planoDerecho.addLast(p.element());
+			i++;
+		}
 	}
 }
